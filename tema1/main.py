721091b1a2cbe910bd182b290db2c83d1e12d97d
George Butco <57345333+George-debug@users.noreply.github.com>
2023-03-03 13:02:14 +0200

    animated plot

--- tema1/main.py
+++ tema1/main.py
@@ -1,8 +1,11 @@
 
+from threading import Thread
 from matplotlib import animation
 import matplotlib.pyplot as plt
 import time
 import random
+
+import numpy as np
 
 
 def get_precition():
@@ -64,20 +67,26 @@ def sub_matrix(A: list[list], B: list[list]):
     return [[A[i][j] - B[i][j] for j in range(len(A))] for i in range(len(A))]
 
 
-def strassen_mul_power_2(A: list[list], B: list[list]):
-    if len(A) == 1:
-        return [[A[0][0] * B[0][0]]]
+def strassen_mul_power_2(A: list[list], B: list[list], n_min: int):
+
+    if len(A) <= n_min:
+        return simple_mul(A, B)
+
+    # print("start", len(A))
 
     A11, A12, A21, A22 = split_matrix(A)
     B11, B12, B21, B22 = split_matrix(B)
 
-    P1 = strassen_mul_power_2(add_matrix(A11, A22), add_matrix(B11, B22))
-    P2 = strassen_mul_power_2(add_matrix(A21, A22), B11)
-    P3 = strassen_mul_power_2(A11, sub_matrix(B12, B22))
-    P4 = strassen_mul_power_2(A22, sub_matrix(B21, B11))
-    P5 = strassen_mul_power_2(add_matrix(A11, A12), B22)
-    P6 = strassen_mul_power_2(sub_matrix(A21, A11), add_matrix(B11, B12))
-    P7 = strassen_mul_power_2(sub_matrix(A12, A22), add_matrix(B21, B22))
+    P1 = strassen_mul_power_2(add_matrix(
+        A11, A22), add_matrix(B11, B22), n_min)
+    P2 = strassen_mul_power_2(add_matrix(A21, A22), B11, n_min)
+    P3 = strassen_mul_power_2(A11, sub_matrix(B12, B22), n_min)
+    P4 = strassen_mul_power_2(A22, sub_matrix(B21, B11), n_min)
+    P5 = strassen_mul_power_2(add_matrix(A11, A12), B22, n_min)
+    P6 = strassen_mul_power_2(sub_matrix(
+        A21, A11), add_matrix(B11, B12), n_min)
+    P7 = strassen_mul_power_2(sub_matrix(
+        A12, A22), add_matrix(B21, B22), n_min)
 
     C11 = add_matrix(sub_matrix(add_matrix(P1, P4), P5), P7)
     C12 = add_matrix(P3, P5)
@@ -92,6 +101,8 @@ def strassen_mul_power_2(A: list[list], B: list[list]):
             C[i + len(C) // 2][j] = C21[i][j]
             C[i + len(C) // 2][j + len(C) // 2] = C22[i][j]
 
+    # print("end", len(A))
+
     return C
 
 
@@ -103,7 +114,7 @@ def next_power_of_2(n: int):
     return i
 
 
-def strassen_mul_square(A: list[list], B: list[list]):
+def strassen_mul_square(A: list[list], B: list[list], n_min=4):
     n = len(A)
     m = next_power_of_2(n)
 
@@ -115,7 +126,7 @@ def strassen_mul_square(A: list[list], B: list[list]):
             A_copy[i][j] = A[i][j]
             B_copy[i][j] = B[i][j]
 
-    C = strassen_mul_power_2(A_copy, B_copy)
+    C = strassen_mul_power_2(A_copy, B_copy, n_min=n_min)
 
     # Remove padding
     for i in range(n):
@@ -152,14 +163,15 @@ def create_matrix(n: int):
 
 strassen_lines = []
 simple_lines = []
+np_lines = []
 
 
-def get_test(n):
+def get_test(n, n_min=1):
     A = create_matrix(n)
     B = create_matrix(n)
 
     start = time.time()
-    strassen_mul_square(A, B)
+    strassen_mul_square(A, B, n_min=n_min)
     end = time.time()
     strassen_time = end - start
 
@@ -168,13 +180,42 @@ def get_test(n):
     end = time.time()
     simple_time = end - start
 
-    return strassen_time, simple_time
+    start = time.time()
+    np.matmul(A, B)
+    end = time.time()
+    np_time = end - start
+
+    return strassen_time, simple_time, np_time
 
 
-def create_plot_test():
+class PlotAnimateWrapper:
+    def __init__(self, animate_func, max_counter):
+        self.animate_func = animate_func
+        self.runningThread = None
+        self.counter = 0
+        self.max_counter = max_counter
+
+    def __call__(self, *args, **kwargs):
+        if self.counter >= self.max_counter:
+            return None
+        if self.runningThread is None or not self.runningThread.is_alive():
+            if len(args) > 0:
+                args = list(args)
+                args[0] = self.counter
+                args = tuple(args)
+                self.counter += 1
+            self.runningThread = Thread(
+                target=self.animate_func, args=args, kwargs=kwargs)
+            self.runningThread.start()
+            return self.runningThread
+        return self.runningThread
+
+
+def create_plot_test(max_i=10, n_min=8):
     global strassen_lines, simple_lines
     strassen_lst = []
     simple_lst = []
+    np_lst = []
 
     fig, ax = plt.subplots()
 
@@ -184,18 +225,22 @@ def create_plot_test():
 
     strassen_lines = ax.plot(strassen_lst, label='Strassen', color='red')
     simple_lines = ax.plot(simple_lst, label='Simple', color='blue')
+    np_lines = ax.plot(np_lst, label='Numpy', color='green')
 
     ax.legend()
 
     def animate(i):
-        global strassen_lines, simple_lines
+        global strassen_lines, simple_lines, np_lines
         n = i + 1
-        n *= 1
+        # n *= 10
+        # n = 2**(i//2) if i % 2 == 0 else 2**(i//2) + 1
+        # n = 2**i
 
-        strassen_time, simple_time = get_test(n)
+        strassen_time, simple_time, np_time = get_test(n, n_min=n_min)
 
         strassen_lst.append((n, strassen_time))
         simple_lst.append((n, simple_time))
+        np_lst.append((n, np_time))
 
         # delete old lines
 
@@ -205,20 +250,26 @@ def create_plot_test():
         for line in simple_lines:
             line.remove()
 
+        for line in np_lines:
+            line.remove()
+
         # draw new lines
         strassen_lines = ax.plot(
             *zip(*strassen_lst), label='Strassen', color='red')
         simple_lines = ax.plot(*zip(*simple_lst), label='Simple', color='blue')
+        np_lines = ax.plot(*zip(*np_lst), label='Numpy', color='green')
 
-    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    animateWrapper = PlotAnimateWrapper(animate, max_i)
+
+    ani = animation.FuncAnimation(fig, animateWrapper, interval=2000)
 
     plt.show()
 
 
 if __name__ == '__main__':
-    # create_plot_test()
+    create_plot_test(max_i=100, n_min=8)
 
-    strassen_time, simple_time = get_test(1000)
+    # strassen_time, simple_time = get_test(1000, n_min=4)
 
-    print("Strassen time: ", strassen_time)
-    print("Simple time: ", simple_time)
+    # print("Strassen time: ", strassen_time)
+    # print("Simple time: ", simple_time)
